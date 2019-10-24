879320ec523e5cbed2d9fbdb99d56fe63e58a4dc
parveenkhan786 <56950494+parveenkhan786@users.noreply.github.com>
2019-10-23 22:51:23 -0700

    Add files via upload

---
+++ final work.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	for (i=0;i<=7;i++)
+{
+	int day;
+	printf ("Enter Number of Day\n");
+	scanf("%d",&day);
+
+if(day==1){
+	printf("first day monday (ICT Sir Asfandyar)\n");
+}
+if(day==2){
+	printf("second day tuseday (Programmming Fundamental Sir Imran)\n");
+}
+if(day==3){
+	printf("third day wednesday (English mir Sadaat Baloch)\n");
+}
+if(day==4){
+	printf("fourth day thursday (Applied Physics Sir Shamsullah)\n");
+}
+if(day==5){
+	printf("fifth day friday (Mathmathics Sir Naveed)\n");
+}
+if(day==6){
+	printf("sixth day saturday (holiday)\n");
+}
+if(day==7){
+	printf("seventh day sunday (holiday)\n");
+}
+}
+	return 0;
+}
